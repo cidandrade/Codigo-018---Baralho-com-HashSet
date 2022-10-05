cab2244b104d2ce041bf41d79a4ff3c2ce0866d6
Cid R. Andrade <profandrade@gmail.com>
2022-10-04 21:59:22 -0300

    Versão 1.0.22.10.04b Set Reorganização

--- src/br/com/cidandrade/aulas/Baralho.java
+++ src/br/com/cidandrade/aulas/Baralho.java
@@ -2,76 +2,34 @@ package br.com.cidandrade.aulas;
 
 import br.com.cidandrade.aulas.enums.Face;
 import br.com.cidandrade.aulas.enums.Naipe;
-import br.com.cidandrade.aulas.interfaces.CartasManip;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class Baralho implements CartasManip {
-
-    Set<Carta> baralho;
-
-    @Override
-    public boolean contemCarta(Carta carta) {
-        return baralho.contains(carta);
-    }
-
-    @Override
-    public boolean contemCarta(Face face, Naipe naipe) {
-        return contemCarta(new Carta(face, naipe));
-    }
-
-    @Override
-    public void removeCarta(Carta carta) {
-        baralho.remove(carta);
-    }
-
-    @Override
-    public void removeCarta(Face face, Naipe naipe) {
-        removeCarta(new Carta(face, naipe));
-    }
-
-    @Override
-    public int quantasCartas() {
-        return baralho.size();
-    }
-
-    @Override
-    public void esvaziar() {
-        baralho.clear();
-    }
-
-    @Override
-    public boolean estaVazio() {
-        return baralho.isEmpty();
-    }
-
-    @Override
-    public void imprime() {
-        baralho.forEach(System.out::println);
-    }
+public class Baralho extends ConjuntoDeCartas {
 
     public Mao retiraCartas(int qtde) {
         Set<Carta> mao = new HashSet<>();
         Mao retorno = null;
-        if (qtde <= baralho.size()) {
-            Iterator<Carta> i = baralho.iterator();
+        if (qtde <= conjunto.size()) {
+            Iterator<Carta> i = conjunto.iterator();
             while (i.hasNext() && qtde > 0) {
                 mao.add(i.next());
                 qtde--;
             }
             retorno = new Mao(mao);
-            baralho.removeAll(mao);
+            conjunto.removeAll(mao);
         }
         return retorno;
     }
 
     public Baralho() {
-        baralho = new HashSet<>();
+        conjunto = new HashSet<>();
         for (Naipe naipe : Naipe.values()) {
             for (Face face : Face.values()) {
-                baralho.add(new Carta(face, naipe));
+                conjunto.add(new Carta(face, naipe));
             }
         }
     }
+
 }
--- src/br/com/cidandrade/aulas/Mao.java
+++ src/br/com/cidandrade/aulas/Mao.java
@@ -1,58 +1,20 @@
 package br.com.cidandrade.aulas;
 
-import br.com.cidandrade.aulas.enums.Face;
-import br.com.cidandrade.aulas.enums.Naipe;
-import br.com.cidandrade.aulas.interfaces.CartasManip;
 import br.com.cidandrade.util.Mensagem;
 import java.util.Set;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
-public class Mao implements CartasManip {
-
-    Set<Carta> mao;
-
-    @Override
-    public boolean contemCarta(Carta carta) {
-        return mao.contains(carta);
-    }
-
-    @Override
-    public boolean contemCarta(Face face, Naipe naipe) {
-        return contemCarta(new Carta(face, naipe));
-    }
-
-    @Override
-    public void removeCarta(Carta carta) {
-        mao.remove(carta);
-    }
-
-    @Override
-    public void removeCarta(Face face, Naipe naipe) {
-        removeCarta(new Carta(face, naipe));
-    }
-
-    @Override
-    public int quantasCartas() {
-        return mao.size();
-    }
-
-    @Override
-    public void esvaziar() {
-        mao.clear();
-    }
-
-    @Override
-    public boolean estaVazio() {
-        return mao.isEmpty();
-    }
+public class Mao extends ConjuntoDeCartas {
 
     @Override
     public void imprime() {
         StringBuilder sb = new StringBuilder();
-        mao.forEach((c) -> sb.append(c).append(", "));
+        conjunto.forEach((c) -> sb.append(c).append(", "));
         String listaMao = sb.substring(0, sb.length() - 2);
         Mensagem.mensagem(listaMao);
+    }
+
+    public Mao(Set<Carta> conjunto) {
+        this.conjunto = conjunto;
     }
 
 }
